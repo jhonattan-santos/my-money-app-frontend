c3ca91cb72587ff25703697d5fbd3e8ded1e1790
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2020-03-24 10:26:48 -0300

    Change uri of api and generate build

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,9 @@
 export default {
-    API_URL: 'http://localhost:3003/api',
-    OAPI_URL: 'http://localhost:3003/oapi',
+    API_URL: 'https://mm-app-back-node.herokuapp.com/api',
+    OAPI_URL: 'https://mm-app-back-node.herokuapp.com/oapi',
 }
+
+// export default {
+//     API_URL: 'http://localhost:3003/api',
+//     OAPI_URL: 'http://localhost:3003/oapi',
+// }
